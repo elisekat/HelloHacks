9ea77a111aa37d02e4876c41508559a16bb8334a
Sumant <a9w8a@ugrad.cs.ubc.ca>
2022-09-25 11:39:02 -0700

    added update function

--- src/assignments.js
+++ src/assignments.js
@@ -15,14 +15,44 @@ export function createAssignments(input) {
             Term: input.term,
             Posts: []
         };
-       assignments.push(newAssignment);
-       return assignments;
-       }}
+        assignments.push(newAssignment);
+        return assignments;
+    }
+}
 
 //add posts by updating assignments 
-export function updateAssignments(input){
+export function updateAssignments(input) {
 
 
 }
+
+// UPDATE FUNCTION
+// Within the addReview page, these changes will be added to an assignment
+
+export function updateAssignments(input) {
+    const index = assignments.findIndex((assignment) => {
+        return assignment.name === input.na, e;
+    });
+    if (index === -1) {
+        return assignments;
+    }
+    const assignment = assignments[index];
+
+    if (input.difficulty) {
+        assignment.posts.difficulty = Number(input.difficulty);
+    }
+    if (input.time_spent) {
+        assignment.posts.time = number(input.time_spent);
+    }
+    if (input.Comment) {
+        assignment.posts.Comment = string(input.Comment);
+    }
+
+    assignemnts[index] = assignment;
+    return assignments;
+}
+
+
+
 
 
--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,6 @@ import {
 
 const showResult = document.getElementById("result");
 const createAssignmentsForm = document.getElementById("createAssignmentsForm");
-const readAssignmentsForm = document.getElementById("readAssignmentsForm");
 const updateAssignmentsForm = document.getElementById("updateAssignmentsForm");
 
 createAssignmentsForm.addEventListener("submit", function (event) {
@@ -15,4 +14,12 @@ createAssignmentsForm.addEventListener("submit", function (event) {
     const result = createAssignments(formProps);
     showResult.innerHTML = JSON.stringify(result, null, 2);
 });
+
+updateAssignmentsForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formProps = Object.fromEntries(formData);
+    const result = updateAssignments(formProps);
+    showResult.innerHTML = JSON.stringify(result, null, 2);
+  });
 
