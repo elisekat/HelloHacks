da48cb152c07c642a101f9bbe735812a792a8936
Elise <elisekayteu@outlook.com>
2022-09-24 22:55:34 -0700

    commit fixed bugs!!! yay yay

--- src/assignments.js
+++ src/assignments.js
@@ -1,4 +1,4 @@
-import data from "../data.json";
+import data from "./data.json" assert {type: "json"};
 let assignments = data;
 
 // CREATE FUNCTION
--- src/handler.js
+++ src/handler.js
@@ -13,5 +13,5 @@ createAssignmentsForm.addEventListener("submit", function (event) {
     const formData = new FormData(event.target);
     const formProps = Object.fromEntries(formData);
     const result = createAssignments(formProps);
-    showResult.innerHTML = JSON.stringify(result, nul, 2);
+    showResult.innerHTML = JSON.stringify(result, null, 2);
 });
